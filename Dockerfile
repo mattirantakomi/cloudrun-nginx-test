69918e8198ac1e3498ca8e45ccb47c125fd8c435
Matti Rantakömi <matti@rantakomi.fi>
2022-05-31 10:04:38 +0300

    asdf

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-FROM ubuntu:22.04
+FROM alpine:3.11
 
-RUN apt-get update && apt-get install -y curl
-RUN curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-RUN curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+RUN apk add --no-cache \
+  bash
 
-RUN apt-get update && apt-get install -y wget screen git dnsutils iputils-ping traceroute nano tailscale nginx dropbear net-tools
-
+COPY --from=jpillora/chisel /app/chisel /usr/bin
 COPY layers/ /
 
-RUN ln -sf /dev/stdout /var/log/nginx/access.log && ln -sf /dev/stderr /var/log/nginx/error.log
-RUN rm -f /etc/nginx/sites-enabled/default && ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled/
-
-ENTRYPOINT ["/entrypoint.sh"]
+ENTRYPOINT [ "/entrypoint.sh" ]
--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -1,49 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-if [ ! -z "${TAILSCALE_AUTHKEY}" ]; then
-    tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --debug 0.0.0.0:8081 &
-    tailscale up --ssh=true --authkey=${TAILSCALE_AUTHKEY} --hostname=cloudrun-testi
-    echo "tailscale started"
-fi
+_term() {
+  >&2 echo "TERM (entrypoint.sh)"
+  exit 0
+}
+trap "_term" TERM
 
-CONF_DIR="/etc/dropbear"
-SSH_KEY_DSS="${CONF_DIR}/dropbear_dss_host_key"
-SSH_KEY_RSA="${CONF_DIR}/dropbear_rsa_host_key"
+echo "{\"<$CHISEL_USER:$CHISEL_PASS>\": [\"\"]}" > /root/users.json
 
-# Check if conf dir exists
-if [ ! -d ${CONF_DIR} ]; then
-    mkdir -p ${CONF_DIR}
-fi
-chown root:root ${CONF_DIR}
-chmod 755 ${CONF_DIR}
+while true; do
+  set +e
+    chisel server -v --authfile /root/users.json --port $PORT --reverse
+    # chisel server --port $PORT --reverse
+  set -e
+  sleep 1
+done
 
-# Check if keys exists
-if [ ! -f ${SSH_KEY_DSS} ]; then
-    dropbearkey  -t dss -f ${SSH_KEY_DSS}
-fi
-chown root:root ${SSH_KEY_DSS}
-chmod 600 ${SSH_KEY_DSS}
-
-if [ ! -f ${SSH_KEY_RSA} ]; then
-    dropbearkey  -t rsa -f ${SSH_KEY_RSA} -s 2048
-fi
-chown root:root ${SSH_KEY_RSA}
-chmod 600 ${SSH_KEY_RSA}
-
-if [ ! -z "${AUTHORIZED_KEYS}" ]; then
-  mkdir -p /root/.ssh
-  echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
-  chmod 700 /root/.ssh
-  chmod 600 /root/.ssh/authorized_keys
-fi
-
-/usr/sbin/dropbear -j -k -s -E
-echo "dropbear started"
-
-echo -n "tailscale ip is "
-tailscale ip
-
-/usr/sbin/nginx -g "daemon off;"
-echo "nginx started"
-
-#tail -f /dev/null
+done
